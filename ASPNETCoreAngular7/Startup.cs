aeee27737eaf0ce8ded7d942156ecc4492251eb5
Bharat <bharat.rsm@gmail.com>
2022-10-08 23:46:12 +0530

    changes for prod mode

--- ASPNETCoreAngular7/Startup.cs
+++ ASPNETCoreAngular7/Startup.cs
@@ -23,10 +23,10 @@ namespace ASPNETCoreAngular7
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
-            //services.AddSpaStaticFiles(configuration =>
-            //{
-            //    configuration.RootPath = "ClientApp/dist";
-            //});
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +44,7 @@ namespace ASPNETCoreAngular7
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            //app.UseSpaStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseMvc(routes =>
             {
