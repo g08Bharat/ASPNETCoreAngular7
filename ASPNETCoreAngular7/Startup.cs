86a622528dacebb66aafeed3522562efdf014895
g08Bharat <108625955+g08Bharat@users.noreply.github.com>
2022-10-14 22:03:59 +0530

    Update Startup.cs

--- ASPNETCoreAngular7/Startup.cs
+++ ASPNETCoreAngular7/Startup.cs
@@ -42,14 +42,17 @@ namespace ASPNETCoreAngular7
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {                
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+            //if (env.IsDevelopment())
+            //{                
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //}
+            
+            app.UseDeveloperExceptionPage();
+            app.UseMigrationsEndPoint();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
